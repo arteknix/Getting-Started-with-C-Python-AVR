c46d34bfb7e8707910a660026f7e189ce8beea2b
arteknix <arteknix@gmail.com>
2025-10-25 00:21:29 +0200

    Just ading the minimal.c file for test

---
+++ minimal.c
@@ -0,0 +1,31 @@
+/* ©arteknix : code in C/minimal.c 
+ * simplest C program
+ * using printf (man printf)
+ * to print a string to stdout (terminal)
+ * 
+ * compile it:  gcc minimal.c -o minimal 
+ * run it:	./minimal */
+
+/* header files 
+ * std 	= standard
+ * io 	= input/output */
+
+#include <stdio.h> //printf 
+
+/* main program: compulsary
+ * has to return an "int" for exit status */
+
+int main(){
+	printf("\nHello C\n");	// "\n" means newline
+	return 0;
+}
+
+/* Notes :
+ * two types of comments 
+ * */
+
+/* ***********************
+ * Traditional C comments, 
+ * multiline 
+ * ***********************/
+// C++ comments. single line
