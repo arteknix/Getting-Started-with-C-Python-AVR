97d36f00ed6f1cd57e94c8ad75a1d16d18367e30
Andréas <arteknix@gmail.com>
2025-10-28 14:48:31 +0100

    Add minimal program with command-line arguments
    This program prints a greeting and the number of command-line arguments.

---
+++ LearningC/Lessons/00_Start/minimal_w_args.c
@@ -0,0 +1,9 @@
+/* arteknix
+this is the version of minimal.c with command-line arguments
+the official/historical way for main
+*/
+#include <stdio.h>
+int main(int argc, char** argv){
+  printf("Hello C!\n%d args, for program %s", argc, argv[0]);
+  return 0;
+}
