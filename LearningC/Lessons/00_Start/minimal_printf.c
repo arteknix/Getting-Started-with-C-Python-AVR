34cb9c814f43abbf97d7d330cf6094a236156b7c
Andréas <arteknix@gmail.com>
2025-10-28 15:42:23 +0100

    Create minimal_printf.c
    Printf example with string, int and float.

---
+++ LearningC/Lessons/00_Start/minimal_printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void){
+  /*variable declarations and initialisations*/
+  int   age   = 123;
+  float pi    = 3.14159265358979;
+  char* name  = "bugs bunny";
+  /**/
+  printf("My name is %s, and I'm %d years old. I love %f", name, age, pi);
+  return 0;
+}
