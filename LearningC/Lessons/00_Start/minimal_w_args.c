0844523e8abf00e4bddc4a50c7daa55f8f257385
arteknix <arteknix@gmail.com>
2025-10-29 12:00:55 +0100

    Update minimal_w_args.c

--- LearningC/Lessons/00_Start/minimal_w_args.c
+++ LearningC/Lessons/00_Start/minimal_w_args.c
@@ -3,7 +3,7 @@ this is the version of minimal.c with command-line arguments
 the official/historical way for main
 */
 #include <stdio.h>
-int main(int argc, char** argv){
+int main(int argc, char* argv[]){
   printf("Hello C!\n%d args, for program %s", argc, argv[0]);
   return 0;
 }
