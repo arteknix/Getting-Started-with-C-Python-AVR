7fc1ef4c0075c0e56a6c087fdd68e3478d29ee25
arteknix <arteknix@gmail.com>
2025-11-06 19:28:30 +0100

    code for i2c oled 0.96" display using SSD1306
    I created a header file for the graphics functions, as the compiler was complaining
    Usage with Arduino-Mk

---
+++ UsingArduino/Arduino-Make/ssd1306_128x64_i2c/ssd1306_128x64_fcts.h
@@ -0,0 +1,20 @@
+//ssd1306_128x64_fcts.h
+#ifndef SSD1306_FCTS_H
+#define SSD1306_FCTS_H 1
+
+void testdrawline() ;
+void testdrawrect(void) ;
+void testfillrect(void) ;
+void testdrawcircle(void) ;
+void testfillcircle(void) ;
+void testdrawroundrect(void) ;
+void testfillroundrect(void) ;
+void testdrawtriangle(void) ;
+void testfilltriangle(void) ;
+void testdrawchar(void) ;
+void testdrawstyles(void) ;
+void testscrolltext(void) ;
+void testdrawbitmap(void) ;
+void testanimate(const uint8_t *bitmap, uint8_t w, uint8_t h) ;
+
+#endif
